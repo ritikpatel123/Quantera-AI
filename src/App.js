19ea8fe274e54d3065c0d7df1aee908e8e220627
Gagan <gaganshuk@gmail.com>
2023-09-12 11:57:11 +0530

    landing page 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from './component/Home-Page/Home';
 import './App.css';
 import Landing from './component/landing-page-02/landing';
+import Landing03 from './component/landing-page-03/landing03';
 
 
 function App() {
   return (
     //  <Home/>
-    <Landing/>
+    // <Landing/>
+    <Landing03/>
      
 
   );
